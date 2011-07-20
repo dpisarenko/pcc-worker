e11edfa0d182a4773cdcffef46cac36b3a9b31b4
Dmitri Pisarenko <dp@sw-dev.at>
2011-07-20 10:45:59 +0200

    Implemented method co.altruix.pcc.impl.immediatereschedulingrequestprocessor.DefaultImmediateSchedulingRequestMessageProcessor.exportDataToGoogleCalendar(UserData)

--- src/main/java/co/altruix/pcc/impl/immediatereschedulingrequestprocessor/DefaultImmediateSchedulingRequestMessageProcessor.java
+++ src/main/java/co/altruix/pcc/impl/immediatereschedulingrequestprocessor/DefaultImmediateSchedulingRequestMessageProcessor.java
@@ -12,6 +12,8 @@
 package co.altruix.pcc.impl.immediatereschedulingrequestprocessor;
 
 import java.io.IOException;
+import java.net.URL;
+import java.security.PrivateKey;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
@@ -22,11 +24,15 @@ import org.slf4j.LoggerFactory;
 import at.silverstrike.pcc.api.export2tj3.InvalidDurationException;
 import at.silverstrike.pcc.api.gcaltasks2pcc.GoogleCalendarTasks2PccImporter;
 import at.silverstrike.pcc.api.gcaltasks2pcc.GoogleCalendarTasks2PccImporterFactory;
+import at.silverstrike.pcc.api.model.Booking;
 import at.silverstrike.pcc.api.model.Resource;
 import at.silverstrike.pcc.api.model.SchedulingObject;
 import at.silverstrike.pcc.api.model.UserData;
 import at.silverstrike.pcc.api.persistence.Persistence;
+import at.silverstrike.pcc.api.privatekeyreader.PrivateKeyReader;
+import at.silverstrike.pcc.api.privatekeyreader.PrivateKeyReaderFactory;
 import at.silverstrike.pcc.api.projectscheduler.ProjectScheduler;
+import at.silverstrike.pcc.impl.privatekeyreader.DefaultPrivateKeyReaderFactory;
 
 import com.google.api.client.auth.oauth2.draft10.AccessTokenResponse;
 import com.google.api.client.googleapis.auth.oauth2.draft10.GoogleAccessProtectedResource;
@@ -35,6 +41,18 @@ import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson.JacksonFactory;
 import com.google.api.services.tasks.v1.Tasks;
+import com.google.gdata.client.authn.oauth.GoogleOAuthParameters;
+import com.google.gdata.client.authn.oauth.OAuthException;
+import com.google.gdata.client.authn.oauth.OAuthRsaSha1Signer;
+import com.google.gdata.client.calendar.CalendarService;
+import com.google.gdata.data.DateTime;
+import com.google.gdata.data.PlainTextConstruct;
+import com.google.gdata.data.calendar.CalendarEntry;
+import com.google.gdata.data.calendar.CalendarEventEntry;
+import com.google.gdata.data.calendar.CalendarEventFeed;
+import com.google.gdata.data.calendar.CalendarFeed;
+import com.google.gdata.data.extensions.When;
+import com.google.gdata.util.ServiceException;
 import com.google.inject.Injector;
 
 import ru.altruix.commons.api.di.PccException;
@@ -84,7 +102,7 @@ class DefaultImmediateSchedulingRequestMessageProcessor implements
 
         importDataFromGoogleTasks(userData);
         calculatePlan(userData);
-        exportDataToGoogleCalendar();
+        exportDataToGoogleCalendar(userData);
 
         LOGGER.debug(
                 "Immediate rescheduling request for user {}, processing finished",
@@ -142,9 +160,140 @@ class DefaultImmediateSchedulingRequestMessageProcessor implements
         }
     }
 
-    private void exportDataToGoogleCalendar() {
-        // TODO Auto-generated method stub
+    private void exportDataToGoogleCalendar(final UserData aUser) {
+        try {
+            final PrivateKey privKey = getPrivateKey();
+            final OAuthRsaSha1Signer signer = new OAuthRsaSha1Signer(privKey);
 
+            GoogleOAuthParameters oauthParameters = new GoogleOAuthParameters();
+            oauthParameters.setOAuthConsumerKey(PCCHQ_COM);
+
+            oauthParameters.setScope("https://www.google.com/calendar/feeds");
+
+            oauthParameters.setOAuthVerifier(aUser
+                    .getGoogleCalendarOAuthVerifier()); // Verifier from the
+                                                        // interactive part
+            oauthParameters.setOAuthToken(aUser.getGoogleCalendarOAuthToken()); // Access
+                                                                                // token
+                                                                                // from
+                                                                                // the
+                                                                                // interactive
+                                                                                // part
+            oauthParameters.setOAuthTokenSecret(aUser
+                    .getGoogleCalendarOAuthTokenSecret()); // Token secret from
+                                                           // the interactive
+                                                           // part
+
+            final CalendarService calendarService =
+                    new CalendarService(PCCHQ_COM);
+
+            calendarService
+                    .setOAuthCredentials(oauthParameters, signer);
+
+            final URL feedUrl =
+                    new URL(
+                            "http://www.google.com/calendar/feeds/default/allcalendars/full");
+            final CalendarFeed resultFeed =
+                    calendarService.getFeed(feedUrl, CalendarFeed.class);
+
+            LOGGER.debug("resultFeed: {}", resultFeed);
+
+            LOGGER.debug("Your calendars:");
+
+            CalendarEntry pccCalendar = null;
+            for (int i = 0; (i < resultFeed.getEntries().size())
+                    && (pccCalendar == null); i++) {
+                final CalendarEntry entry = resultFeed.getEntries().get(i);
+
+                if ("PCC".equals(entry.getTitle().getPlainText())) {
+                    pccCalendar = entry;
+                }
+            }
+
+            // Delete all events in the PCC calendar
+
+            LOGGER.debug(
+                    "PCC calendar: edit link='{}', self link='{}', content='{}', id='{}'",
+                    new Object[] { pccCalendar.getEditLink().getHref(),
+                            pccCalendar.getSelfLink().getHref(),
+                            pccCalendar.getContent(), pccCalendar.getId() });
+
+            final String calendarId =
+                    pccCalendar
+                            .getId()
+                            .substring(
+                                    "http://www.google.com/calendar/feeds/default/calendars/"
+                                            .length());
+            final URL pccCalendarUrl =
+                    new URL(
+                            "https://www.google.com/calendar/feeds/${calendarId}/private/full"
+                                    .replace("${calendarId}", calendarId));
+
+            LOGGER.debug("pccCalendarUrl: {}", pccCalendarUrl);
+            // calendarService.getFeed(feedUrl, feedClass)
+
+            final CalendarEventFeed pccEventFeed =
+                    calendarService.getFeed(pccCalendarUrl,
+                            CalendarEventFeed.class);
+            for (final CalendarEventEntry curEvent : pccEventFeed.getEntries()) {
+                curEvent.delete();
+            }
+
+            final List<Booking> bookings =
+                    this.persistence.getBookings(aUser);
+
+            LOGGER.debug("Bookings to export: {}", bookings.size());
+
+            for (final Booking curBooking : bookings) {
+                LOGGER.debug(
+                        "Exporting: start date time: {}, end date time: {}",
+                        new Object[] { curBooking.getStartDateTime(),
+                                curBooking.getEndDateTime() });
+
+                final CalendarEventEntry event = new CalendarEventEntry();
+
+                event.setTitle(new PlainTextConstruct(curBooking.getProcess()
+                        .getName()));
+
+                final When eventTime = new When();
+                final DateTime startDateTime =
+                        new DateTime(curBooking.getStartDateTime().getTime());
+                final DateTime endDateTime =
+                        new DateTime(curBooking.getEndDateTime().getTime());
+
+                eventTime.setStartTime(startDateTime);
+                eventTime.setEndTime(endDateTime);
+
+                event.addTime(eventTime);
+
+                calendarService.insert(pccCalendarUrl, event);
+            }
+        } catch (final IOException exception) {
+            LOGGER.error("", exception);
+        } catch (final OAuthException exception) {
+            LOGGER.error("", exception);
+        } catch (final ServiceException exception) {
+            LOGGER.error("", exception);
+        }
+
+    }
+
+    private PrivateKey getPrivateKey() {
+        final PrivateKeyReaderFactory factory =
+                new DefaultPrivateKeyReaderFactory();
+        final PrivateKeyReader reader = factory.create();
+
+        reader.setInputStream(getClass().getClassLoader()
+                        .getResourceAsStream("privatekey"));
+
+        try {
+            reader.run();
+
+            return reader.getPrivateKey();
+        } catch (final PccException exception) {
+            LOGGER.error("", exception);
+            return null;
+        }
     }
 
     private void importDataFromGoogleTasks(final UserData aUserData) {
