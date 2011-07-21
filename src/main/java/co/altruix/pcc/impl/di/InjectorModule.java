6442f9fc1a4749cb694e0a95deb7fbfeae187b28
Dmitri Pisarenko <dp@sw-dev.at>
2011-07-21 18:08:17 +0200

    Attempt at bugfix: Exception in thread "main" com.google.inject.ConfigurationException: Guice configuration errors:
    1) No implementation for at.silverstrike.pcc.api.gtask2pcctaskconverter.GoogleTask2PccTaskConverterFactory was bound.
      while locating at.silverstrike.pcc.api.gtask2pcctaskconverter.GoogleTask2PccTaskConverterFactory
    
    1 error
          at com.google.inject.InjectorImpl.getProvider(InjectorImpl.java:784)
          at com.google.inject.InjectorImpl.getProvider(InjectorImpl.java:743)
          at com.google.inject.InjectorImpl.getInstance(InjectorImpl.java:793)
          at at.silverstrike.pcc.impl.gcaltasks2pccimporter.DefaultGoogleCalendarTasks2PccImporter2.createPccTasks(DefaultGoogleCalendarTasks2PccImporter2.java:198)
          at at.silverstrike.pcc.impl.gcaltasks2pccimporter.DefaultGoogleCalendarTasks2PccImporter2.run(DefaultGoogleCalendarTasks2PccImporter2.java:84)
          at at.silverstrike.pcc.impl.gcaltasks2pcc.DefaultGoogleCalendarTasks2PccImporter.run(DefaultGoogleCalendarTasks2PccImporter.java:80)
          at co.altruix.pcc.impl.immediatereschedulingrequestprocessor.DefaultImmediateSchedulingRequestMessageProcessor.importDataFromGoogleTasks(DefaultImmediateSchedulingRequestMessageProcessor.java:337)
          at co.altruix.pcc.impl.immediatereschedulingrequestprocessor.DefaultImmediateSchedulingRequestMessageProcessor.run(DefaultImmediateSchedulingRequestMessageProcessor.java:103)
          at co.altruix.pcc.impl.dispatcher.DefaultDispatcher.run(DefaultDispatcher.java:54)
          at co.altruix.pcc.PccWorkerApp.run(PccWorkerApp.java:69)
          at co.altruix.pcc.PccWorkerApp.main(PccWorkerApp.java:128)

--- src/main/java/co/altruix/pcc/impl/di/InjectorModule.java
+++ src/main/java/co/altruix/pcc/impl/di/InjectorModule.java
@@ -15,6 +15,7 @@ import at.silverstrike.pcc.api.export2tj3.TaskJuggler3Exporter;
 import at.silverstrike.pcc.api.export2tj3.TaskJuggler3ExporterFactory;
 import at.silverstrike.pcc.api.gcaltasks2pcc.GoogleCalendarTasks2PccImporterFactory;
 import at.silverstrike.pcc.api.gcaltasks2pccimporter.GoogleCalendarTasks2PccImporter2Factory;
+import at.silverstrike.pcc.api.gtask2pcctaskconverter.GoogleTask2PccTaskConverterFactory;
 import at.silverstrike.pcc.api.gtasknoteparser.GoogleTaskNotesParserFactory;
 import at.silverstrike.pcc.api.gtaskrelevance.IsGoogleTaskRelevantCalculatorFactory;
 import at.silverstrike.pcc.api.persistence.Persistence;
@@ -23,6 +24,7 @@ import at.silverstrike.pcc.api.projectscheduler.ProjectSchedulerFactory;
 import at.silverstrike.pcc.impl.export2tj3.DefaultTaskJuggler3ExporterFactory;
 import at.silverstrike.pcc.impl.gcaltasks2pcc.DefaultGoogleCalendarTasks2PccImporterFactory;
 import at.silverstrike.pcc.impl.gcaltasks2pccimporter.DefaultGoogleCalendarTasks2PccImporter2Factory;
+import at.silverstrike.pcc.impl.gtask2pcctaskconverter.DefaultGoogleTask2PccTaskConverterFactory;
 import at.silverstrike.pcc.impl.gtasknoteparser.DefaultGoogleTaskNotesParserFactory;
 import at.silverstrike.pcc.impl.gtaskrelevance.DefaultIsGoogleTaskRelevantCalculatorFactory;
 import at.silverstrike.pcc.impl.persistence.DefaultPersistence;
@@ -75,6 +77,8 @@ class InjectorModule extends AbstractModule {
                 new DefaultIsGoogleTaskRelevantCalculatorFactory());
         bind(GoogleTaskNotesParserFactory.class).toInstance(
                 new DefaultGoogleTaskNotesParserFactory());
+        bind(GoogleTask2PccTaskConverterFactory.class).toInstance(
+                new DefaultGoogleTask2PccTaskConverterFactory());
     }
 
     private TaskJuggler3Exporter getTaskJuggler3Exporter() {
